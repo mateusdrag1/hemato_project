a6e67e81b3cd3af0091a79a9ff7182f80b4b2490
Matthieu Christian <mateus1212122@hotmail.com>
2023-07-18 19:14:13 -0300

    fix: up size to 600 length text

--- src/components/AtlasForm/validate.ts
+++ src/components/AtlasForm/validate.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const createCellFormSchema = z.object({
-  name: z.string().min(3).max(256),
-  morphology: z.string().min(3).max(256),
-  clinical_relevance: z.string().min(3).max(256),
+  name: z.string().min(3).max(128),
+  morphology: z.string().min(3).max(600),
+  clinical_relevance: z.string().min(3).max(600),
   category_id: z.coerce.number(),
   image: z.any(),
 });
